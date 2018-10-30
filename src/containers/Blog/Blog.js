6228b595951bdbec4506cebe572d9422d1e0337a
Selma Delgado <selmadelgado@gmail.com>
2018-10-30 17:47:28 -0500

    184 Render Route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -17,7 +17,8 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact render={() => <h1>Home</h1>}/>
+                <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} />
             </div>
         );
     }
