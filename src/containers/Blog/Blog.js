c1ac2f8a69cc3985e11be834e17f35875b22e860
Selma Delgado <selmadelgado@gmail.com>
2018-10-30 17:00:36 -0500

    184 Setup Routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 //import axios from 'axios';
 import './Blog.css';
 import Posts from './Posts/Posts';
+import { Route } from 'react-router-dom';
+
 
 class Blog extends Component {
     render () {
@@ -15,7 +17,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                <Route path="/" exact render={() => <h1>Home</h1>}/>
             </div>
         );
     }
