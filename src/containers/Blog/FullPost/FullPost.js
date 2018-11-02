465e624320a196f04ccc447b480c132af8aa2c83
Selma Delgado <selmadelgado@gmail.com>
2018-11-01 19:57:24 -0500

    200 Create Dynamic Routes 1 of 2

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -10,6 +10,14 @@ class FullPost extends Component {
 
     componentDidMount () {
         console.log(this.props);
+        
+    }
+
+    componentDidUpdate () {
+
+    }
+
+    loadData () {
         if (this.props.match.params.id) {
             if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id) ) {
                 axios.get('/posts/' + this.props.match.params.id)
