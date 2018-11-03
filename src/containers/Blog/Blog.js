44d5d4ecfe0944f33c606d61c339918c3faf0761
Selma Delgado <selmadelgado@gmail.com>
2018-11-03 14:42:29 -0500

    201 Redirect Requests

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-
-
 
 class Blog extends Component {
     render () {
@@ -36,7 +34,9 @@ class Blog extends Component {
 
                 <Switch>
                     <Route path="/new-post" component={NewPost}/>   
-                    <Route path="/posts" component={Posts}/>   
+                    <Route path="/posts" component={Posts}/>
+                    <Redirect from="/" to="/posts" />
+                    
                 </Switch>
                 
             </div>
