59b62e99a5cb95f2674ace64ed005ea9ed96f871
Selma Delgado <selmadelgado@gmail.com>
2018-11-03 15:14:00 -0500

    205 Handle 404 Case

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -39,10 +39,10 @@ class Blog extends Component {
 
                 <Switch>
                     {this.state.auth ?  <Route path="/new-post" component={NewPost}/> : null}
-                     
                     <Route path="/posts" component={Posts}/>
-                    <Redirect from="/" to="/posts" />
-                    
+                    <Route render ={() => <h1>Sorry, Page Not found</h1>} />
+                    {/* <Redirect from="/" to="/posts" /> */}
+                    {/* <Route path="/" component={Posts} /> */}
                 </Switch>
                 
             </div>
